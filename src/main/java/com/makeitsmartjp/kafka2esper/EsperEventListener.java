941d736824f7b5aeb098f45626ee0d99fe422c31
Hidemasa Togashi <togachiro@gmail.com>
2015-04-28 11:09:40 +0900

    Update for sample

--- src/main/java/com/makeitsmartjp/kafka2esper/EsperEventListener.java
+++ src/main/java/com/makeitsmartjp/kafka2esper/EsperEventListener.java
@@ -11,9 +11,11 @@ public class EsperEventListener implements UpdateListener {
 		System.out.println("EventUpdate: " + event.getUnderlying());
 		try {
 			String avg_response = event.get("avg(upstream_response)").toString();
-			String max_response = event.get("avg(upstream_response)").toString();
+			String max_response = event.get("max(upstream_response)").toString();
+			String num_count = event.get("count(*)").toString();
 			System.out.println("Avg. UpstreamResponseTime: " + avg_response);
 			System.out.println("Max. UpstreamResponseTime: " + max_response);
+			System.out.println("Number of requests: " + num_count);
 		} catch (Exception e) {
 			System.out.println("EventListener Exception: " + e);
 		}
